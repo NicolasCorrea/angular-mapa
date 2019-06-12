67c159071142b7914b199f1bf2558bed6aed413c
Nicolas Correa <ncorrea@garantiascomunitarias.com>
2019-06-12 18:29:43 -0500

    angular mapa con mapbox

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
+// import { AgmCoreModule } from '@agm/core';
+import { NgxMapboxGLModule } from 'ngx-mapbox-gl';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    // AgmCoreModule.forRoot({
+    //   apiKey: 'API_KEY'
+    // })
+    NgxMapboxGLModule.withConfig({
+      accessToken: 'pk.eyJ1IjoibmNvcnJlYSIsImEiOiJjand0OThrZ3EwMTNzNDlsODdzM2Q0NndrIn0.RLg29i2RhDVbT_gC6EKq8g'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  mapbox: {
+    accessToken: 'pk.eyJ1IjoibmNvcnJlYSIsImEiOiJjand0OThrZ3EwMTNzNDlsODdzM2Q0NndrIn0.RLg29i2RhDVbT_gC6EKq8g'
+ }
 };
